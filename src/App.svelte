97c607024c0c889153aba10454bf1e9de9aa9481
tranthientam293 <tranthientam2903@gmail.com>
2024-08-15 19:27:31 +0700

    feat: init project and feedback list

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	import FeedbackList from '@/components/FeedbackList.svelte'
+	import { data } from '@/assets/data'
+
+	let feedback = [...data]
+
+	/** @type {(e: CustomEvent<number>) => void} */
+	function deleteFeedback(e) {
+		const itemId = e.detail
+		feedback = feedback.filter((f) => f.id !== itemId)
+		console.log(e)
+	}
+</script>
+
+<main class="container">
+	<FeedbackList {feedback} on:delete-feedback={deleteFeedback} />
+</main>
---
+++ src/components/FeedbackList.svelte
@@ -0,0 +1,8 @@
+<script>
+	import FeebackItem from './FeebackItem.svelte'
+	export let feedback = []
+</script>
+
+{#each feedback as fb (fb.id)}
+	<FeebackItem item={fb} on:delete-feedback />
+{/each}
